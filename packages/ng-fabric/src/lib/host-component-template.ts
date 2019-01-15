5944b30ea6ae2ffca62dd01c79beaafaec251f04
Eswar Prakash <eswar.prakash@tsg.com>
2019-01-15 09:41:28 +0000

    Commit before rebase

--- packages/ng-fabric/src/lib/host-component-template.ts
+++ packages/ng-fabric/src/lib/host-component-template.ts
@@ -1,2 +1,7 @@
+// Copyright (c) 2018 Eswar Prakash
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 // tslint:disable-next-line:max-line-length
 export const HOST_COMPONENT_TEMPLATE = `<div #host><div [ngStyle]="{ 'display': textContent ? 'inline-block' : 'block' }"><ng-content></ng-content></div></div>`;
